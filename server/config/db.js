d2994fe90c7aa3b21721a7417d83c3670c901ba6
Sarvesh221007 <136876726+Sarvesh221007@users.noreply.github.com>
2025-09-19 02:12:06 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://sarveshsingh8303744279_db_user:xVr6OPgtCfs7uyFr@smarthealth01.eryduze.mongodb.net/smarthealth?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://sarveshsingh8303744279_db_user:xVr6OPgtCfs7uyFr@smarthealth01.eryduze.mongodb.net/?retryWrites=true&w=majority&appName=SmartHealth01");
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
